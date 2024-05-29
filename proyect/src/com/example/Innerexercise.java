e60f258d5a4d75e9579a56c8c29000e4ef3e8f2f
HoracioLicona07 <126303879+HoracioLicona07@users.noreply.github.com>
2024-05-29 14:56:14 -0600

    Add files via upload

---
+++ proyect/src/com/example/Innerexercise.java
@@ -0,0 +1,36 @@
+public package com.example;
+//Convert Roman number as a string to decimal numbers from 1 to 100
+//First we are going to import the necessary libraries in order to complete the task
+import java.util.*;
+import java.util.Map;
+
+//We are going to stablish variables that we are going to use in the program
+class exercise {
+    
+    static {
+        
+        String RomanNumberOne = "I";
+        String RomanNumberFive = "V";
+        String RomanNumberTen = "X";
+        String RomanNumberFifty = "L";
+        String RomanNumberHundred = "C";
+
+    }
+
+    
+}
+
+/**
+ * Innerexercise
+ */
+public class Innerexercise {
+
+    if 
+}
+//We are going to stablish variables that we are going to use in the program
+
+//we are going to create the functions that are going to create the process
+
+//we are going to print the result of the code or the exercise
+
+//Remember that it needs to be from string to integers
